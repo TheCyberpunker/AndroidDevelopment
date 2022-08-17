ef934e6d1bac2f27de0228e0a580ae84b94e82ce
ufo <ufo@localhost.localdomain>
2022-08-17 03:04:19 -0500

    Last Sync: mié 17 ago 2022 03:04:19 -05 (T80)

---
+++ JavaBasics-2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main (String[] args){ // This is a method which takes a parameter or arguments as strings or arrays
+
+        System.out.println("Test");
+
+    }
+    
+}
---
+++ JavaBasics-2/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+
+    public static void main (String[] args){
+
+        // variables
+        // integer
+
+        int age = 20;
+
+        System.out.println(10*age);
+
+        // Double - Float
+
+        double x = 9.0;
+        double y = 3.0;
+
+        System.out.println(x/y);
+    }
+    
+}
